46955d0cffd5ca05f1a26ea7302410a452d5d216
jeojoe <jirat.onaree@gmail.com>
2018-05-06 15:13:50 +0700

    :art: Use styled-components :tada:

--- src/screens/ProfileTab.js
+++ src/screens/ProfileTab.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text } from 'react-native';
+import styled from 'styled-components';
 
 import { Colors } from '../constants';
+
+const UserIcon = styled.Image`
+  borderRadius: 16;
+  height: 32;
+  width: 32;
+  borderWidth: ${props => (props.focused ? 2 : 0)};
+  borderColor: ${Colors.main};
+`;
 
 class ProfileTab extends Component {
   static navigationOptions = {
     header: null,
     title: 'โปรไฟล์',
     tabBarIcon: ({ focused }) => (
-      <Image
+      <UserIcon
         source={{ uri: 'https://scontent.fbkk2-4.fna.fbcdn.net/v/t1.0-9/74990_1046920815350946_5019913966088668875_n.jpg?_nc_cat=0&oh=38e52d518c3d97625ef93f36236feb5f&oe=5B2EDCE7' }}
-        style={[{
-          borderRadius: 16,
-          height: 32,
-          width: 32,
-        }, focused && {
-          borderWidth: 2,
-          borderColor: Colors.main,
-        }]}
+        focused={focused}
       />
     ),
   };
