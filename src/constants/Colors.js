abe649cf69fed2a4f98f46ffe43e9d0e604ced1b
jeojoe <jirat.onaree@gmail.com>
2018-05-06 14:36:35 +0700

    Add Colors constant

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,6 +1,50 @@
-const tintColor = '#2f95dc';
+const tintColor = '#E30560';
 
 export default {
+  // ci
+  main: '#E30560',
+  mainDark: '#B90152',
+  rewardBgRight: '#F572B4',
+  rewardBgLeft: '#EF1D6B',
+
+  // bg & border
+  bgGrey: '#F9F9F9',
+  bgMainLightTop: '#FFF5FA',
+  bgMainLightBottom: '#FFEBF4',
+  bgTopBarLeft: '#FF006F',
+  bgTopBarRight: '#C50950',
+  bgMainTop: '#F30C99',
+  bgMainBottom: '#E30560',
+  border: '#979797',
+  mainGradientTopLeft: 'rgba(252, 11, 105, 0.85)',
+  mainGradientBottomRight: 'rgba(203, 17, 80, 1)',
+  darkPink: '#AC1155',
+
+  hr: '#E5E5E5',
+  // input
+  lightPink: '#FF4A8D',
+  textOnLightPink: '#FFECF3',
+
+  // button
+  buttonLeft: '#FF006F',
+  buttonRight: '#C50950',
+  buttonShadow: 'rgba(95,95,95,50)',
+  buttonShadowOnPink: 'rgba(127,0,51,0.5)',
+  placeActionButtonLeft: '#E30560',
+  placeActionButtonRight: '#F30C99',
+
+  // text
+  textGrey: '#525252',
+  textGreyLighter: '#777',
+  textOnPinkInactive: '#FFA4C6',
+
+  // label
+  rating: '#FFD12E',
+
+  // shadow
+  shadow: 'rgba(129,129,129,.5)',
+
+  // Old
   tintColor,
   tabIconDefault: '#ccc',
   tabIconSelected: tintColor,
