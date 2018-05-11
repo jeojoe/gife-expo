048527fba2d923a32a5d533f7e09cf0dc5d382b6
jeojoe <jirat.onaree@gmail.com>
2018-05-11 22:28:18 +0700

    Add style constants

---
+++ src/constants/Style.js
@@ -0,0 +1,4 @@
+export default {
+  // Horizontal padding
+  padHoriz: 20,
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
 import Colors from './Colors';
 import ActionTypes from './ActionTypes';
+import Style from './Style';
 
 export {
   Colors,
   ActionTypes,
+  Style,
 };
