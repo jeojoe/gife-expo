2c97410f3924628fd7b619db40c999a227fabe7e
jeojoe <jirat.onaree@gmail.com>
2018-05-12 14:54:47 +0700

    Import alert message

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,11 @@
 import Colors from './Colors';
 import ActionTypes from './ActionTypes';
 import Style from './Style';
+import AlertMessages from './AlertMessages';
 
 export {
   Colors,
   ActionTypes,
   Style,
+  AlertMessages,
 };
