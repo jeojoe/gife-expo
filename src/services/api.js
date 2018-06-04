8569551a5271010bbae7f02aa846a174774c9b0e
Jirat Onaree <jirat.onaree@gmail.com>
2018-06-04 14:23:30 +0700

    Fix lambda path

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import Config from '../../app.json';
 
-function genEndpoint(apiId) {
-  return `https://${apiId}/${Config.gife.LAMBDA_URL}/stag`;
+function genEndpoint(path) {
+  return `${Config.aws.LAMBDA_URL}/dev/${path}`;
 }
 
 export function mock() {
@@ -9,12 +9,13 @@ export function mock() {
     .then(res => res.json());
 }
 
-export function get(url) {
-  return fetch(genEndpoint(url));
+export function get(path) {
+  return fetch(genEndpoint(path))
+    .then(res => res.json());
 }
 
-export function post(url, body) {
-  return fetch(genEndpoint(url), {
+export function post(path, body) {
+  return fetch(genEndpoint(path), {
     method: 'POST',
     body,
   })
