b537b734254e106668fd49769e3f6465c99dd173
Jirat Onaree <jirat.onaree@gmail.com>
2018-06-06 17:19:48 +0700

    Add layout constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,10 +2,12 @@ import Colors from './Colors';
 import ActionTypes from './ActionTypes';
 import Style from './Style';
 import AlertMessages from './AlertMessages';
+import Layout from './Layout';
 
 export {
   Colors,
   ActionTypes,
   Style,
   AlertMessages,
+  Layout,
 };
