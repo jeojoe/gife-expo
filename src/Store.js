76600001e1d84e0390e96147c994996683df98ef
Jirat Onaree <jirat.onaree@gmail.com>
2018-06-08 12:49:40 +0700

    Assign global log to Reactotron

--- src/Store.js
+++ src/Store.js
@@ -33,6 +33,19 @@ Reactotron
   .use(networking())
   .connect();
 
+const yeOldeConsoleLog = console.log;
+// make a new one
+console.log = (...args) => {
+  // always call the old one, because React Native does magic swizzling too
+  yeOldeConsoleLog(...args);
+
+  // send this off to Reactotron.
+  Reactotron.display({
+    name: 'CONSOLE.LOG',
+    value: args,
+    preview: args.length > 0 && typeof args[0] === 'string' ? args[0] : null,
+  });
+};
 
 const middlewares = [thunk, logger];
 const reducers = combineReducers({
