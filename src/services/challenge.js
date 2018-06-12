fdc0426335475da77b695bdb5b6809f429dd59c4
Jirat Onaree <jirat.onaree@gmail.com>
2018-06-12 13:40:56 +0700

    Add get challenge service

--- src/services/challenge.js
+++ src/services/challenge.js
@@ -3,3 +3,7 @@ import * as API from './api';
 export function getExplore() {
   return API.get('challenge/explore');
 }
+
+export function getChallenge(id) {
+  return API.get(`challenge/${id}`);
+}
